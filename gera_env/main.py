4be12f2c2e90dce8bfdcbb485f2571aa066a4d0b
Raul Soares <83381403+raulrsoares@users.noreply.github.com>
2023-02-10 18:10:12 +0000

    feat: camelcase nas variaveis

--- gera_env/main.py
+++ gera_env/main.py
@@ -3,7 +3,7 @@ import json
 
 arquivo = "captura.json"
 outputsTF = list()
-global_model = os.environ.get('GLOBAL_MODEL')
+globalModel = os.environ.get('GLOBAL_MODEL')
 
 os.system(f'echo $(terraform output -json) >> {arquivo}')
 with open(arquivo) as file:
@@ -11,13 +11,14 @@ with open(arquivo) as file:
 
 
 def separarJson():
-    quant_array = len(data)
+    quantArray = len(data)
     for keyName in data:
-        outputsTF.insert(quant_array, keyName)
+        outputsTF.insert(quantArray, keyName)
     criarTfvars(outputsTF)
     os.system('terraform fmt --recursive')
-    # os.system(f'cp {global_model}.tfvars teste.tfvars')
-    os.system(f'echo -e $(cat {global_model}.tfvars) >> $(pwd)/../front/terraform.tfvars')
+    # os.system(f'cp {globalModel}.tfvars teste.tfvars')
+    os.system(
+        f'echo -e $(cat {globalModel}.tfvars) >> $(pwd)/../front/terraform.tfvars')
     os.system('cat $(find $(pwd)/../front -name "*.tfvars")')
 
 
@@ -27,9 +28,9 @@ def criarTfvars(procurar: list):
         for value in range(0, tamanho, 1):
             itens = procurar[value]
             res = data[f"{itens}"]["value"]
-            tfvars = open(f"{global_model}.tfvars", '+a')
-            write_model = (f'\n{itens} = \"{res}\"#\\n')
-            tfvars.write(write_model)
+            tfvars = open(f"{globalModel}.tfvars", '+a')
+            writeModel = (f'\n{itens} = \"{res}\"#\\n')
+            tfvars.write(writeModel)
     else:
         print("!!PROBLEMA!!")
 
